f9babdf0728bbbe971b27af7d0ea7895589d0f14
justI6E <zhakengeradilet003@gmail.com>
2023-02-12 18:01:04 +0600

    commit

---
+++ project_reports/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", myMiddleWare(myHandler))
+	http.ListenAndServe("localhost:8080", nil)
+}
+
+func myHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("hello from handler")
+	w.WriteHeader(http.StatusOK)
+}
+
+func myMiddleWare(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		log.Println("hello from middleware")
+		next(w, r)
+	}
+}
